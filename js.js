db296e5434c4ab650c811d49c35d498d1c8930de
inga-kapitonova <71716975+inga-kapitonova@users.noreply.github.com>
2021-01-05 20:19:35 +0200

    cards numbers show up in pairs from 1 to 10

--- js.js
+++ js.js
@@ -2,7 +2,36 @@ $(function () {
   let card = $(".game-card");
   let container = $("#wrapper");
 
-  for (let i = 1; i <= 19; i++) {
+  for (let i = 20; i >= 1; i--) {
+    let toTen = uniqueKey(randNumber(i));
+    if (toTen > 10) {
+      toTen = toTen - 10;
+    }
+
+    card.html(toTen);
     container.append(card.clone());
   }
+
+  card.hide();
 });
+
+function randNumber(n) {
+  return Math.floor(Math.random() * n) + 1;
+}
+
+let cardValues = {};
+function uniqueKey(number) {
+  if (cardValues.lenght == 20) {
+    return 0;
+  }
+  if (number in cardValues) {
+    number = number - 1;
+    if (number == 0) {
+      number = 20;
+    }
+    console.log(cardValues);
+    return uniqueKey(number);
+  }
+  cardValues[number] = null;
+  return number;
+}
